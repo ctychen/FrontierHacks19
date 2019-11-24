232edb27ba84350103c5810b42fe421965bc040c
janhavias <46143407+janhavias@users.noreply.github.com>
2019-11-23 19:55:14 -0800

    Update Humanoid.py

--- Py_Humanoid/Humanoid.py
+++ Py_Humanoid/Humanoid.py
@@ -66,8 +66,8 @@ class Humanoid():
 
         leftThigh.rotateTo(120)   #L ANKLE
         leftKnee.rotateTo(120)  #L ANKLE
-        rightThigh.rotateTo(100)   #L ANKLE
-        rightKnee.rotateTo(100)   #L ANKLE
+        rightThigh.rotateTo(105)   #L ANKLE
+        rightKnee.rotateTo(105)   #L ANKLE
 
         time.sleep(delay)
 
