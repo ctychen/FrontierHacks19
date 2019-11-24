48b8cbd729c050d40c4bb0abd2afb4c79cc4b7c1
janhavias <46143407+janhavias@users.noreply.github.com>
2019-11-23 21:10:36 -0800

    Update server.py

--- Py_Humanoid/server.py
+++ Py_Humanoid/server.py
@@ -99,122 +99,122 @@ def action1(action1):
       message = "walkForward"
       thread = threading.Thread(target=walkForward)
       thread.start()
-      #return message + ' started'  
+      return main.html  
    elif action1 == "turnLeft":
       message = "turnLeft"
       thread = threading.Thread(target=turnLeft)
       thread.start()
-      #return message + ' started' 
+      return main.html  
    elif action1 == "turnRight":
       message = "turnRight"
       thread = threading.Thread(target=turnRight)
       thread.start()
-      #return message + ' started' 
+      return main.html  
    elif action1 == "stand":
       message = "stand"
       thread = threading.Thread(target=stand)
       thread.start()
-      #return message + ' started' 
+      return main.html  
  
 #  left claw
    elif action1 == "openLeftClaw":
       message = "openLeftClaw"
       thread = threading.Thread(target=openLeftClaw)
       thread.start()
-      #return message + ' started' 
+      return main.html  
    elif action1 == "closeLeftClaw":
       message = "closeLeftClaw"
       thread = threading.Thread(target=closeLeftClaw)
       thread.start()
-      #return message + ' started' 
+      return main.html  
 
    elif action1 == "turnLeftWristRight":
       message = "turnLeftWristRight"
       thread = threading.Thread(target=turnLeftWristRight)
       thread.start()
-      #return message + ' started' 
+      return main.html  
    elif action1 == "turnLeftWristLeft":
       message = "turnLeftWristLeft"
       thread = threading.Thread(target=turnLeftWristLeft)
       thread.start()
-      #return message + ' started' 
+      return main.html  
 
    elif action1 == "openLeftElbow":
       message = "openLeftElbow"
       thread = threading.Thread(target=openLeftElbow)
       thread.start()
-      #return message + ' started'
+      return main.html  
    elif action1 == "closeLeftElbow":
       message = "closeLeftElbow"
       thread = threading.Thread(target=closeLeftElbow)
       thread.start()
-      #return message + ' started'
+      return main.html  
 
    elif action1 == "raiseLeftShoulder":
       message = "raiseLeftShoulder"
       thread = threading.Thread(target=raiseLeftShoulder)
       thread.start()
-      #return message + ' started'
+      return main.html  
    elif action1 == "midRaiseLeftShoulder":
       message = "midRaiseLeftShoulder"
       thread = threading.Thread(target=midRaiseLeftShoulder)
       thread.start()
-      #return message + ' started'
+      return main.html  
    elif action1 == "lowerLeftShoulder":
       message = "lowerLeftShoulder"
       thread = threading.Thread(target=lowerLeftShoulder)
       thread.start()
-      #return message + ' started'
+      return main.html  
 
 # right claw
    elif action1 == "openRightClaw":
       message = "openRightClaw"
       thread = threading.Thread(target=openRightClaw)
       thread.start()
-      #return message + ' started' 
+      return main.html  
    elif action1 == "closeRightClaw":
       message = "closeRightClaw"
       thread = threading.Thread(target=closeRightClaw)
       thread.start()
-      #return message + ' started' 
+      return main.html  
 
    elif action1 == "turnRightWristRight":
       message = "turnRightWristRight"
       thread = threading.Thread(target=turnRightWristRight)
       thread.start()
-      #return message + ' started' 
+      return main.html  
    elif action1 == "turnRightWristLeft":
       message = "turnRightWristLeft"
       thread = threading.Thread(target=turnRightWristLeft)
       thread.start()
-      #return message + ' started' 
+      return main.html  
 
    elif action1 == "openRightElbow":
       message = "openRightElbow"
       thread = threading.Thread(target=openRightElbow)
       thread.start()
-      #return message + ' started'
+      return main.html  
    elif action1 == "closeRightElbow":
       message = "closeRightElbow"
       thread = threading.Thread(target=closeRightElbow)
       thread.start()
-      #return message + ' started'
+      return main.html  
 
    elif action1 == "raiseRightShoulder":
       message = "raiseRightShoulder"
       thread = threading.Thread(target=raiseRightShoulder)
       thread.start()
-      #return message + ' started'
+      return main.html  
    elif action1 == "midRaiseRightShoulder":
       message = "midRaiseRightShoulder"
       thread = threading.Thread(target=midRaiseRightShoulder)
       thread.start()
-      #return message + ' started'
+      return main.html  
    elif action1 == "lowerRightShoulder":
       message = "lowerRightShoulder"
       thread = threading.Thread(target=lowerRightShoulder)
       thread.start()
-      #return message + ' started'
+      return main.html  
    else:
       message = ""
 
